88e6356fbd90a1e7e6ed3785490df49f4e97b4fd
ZhangXuecheng <zxc0gary@connect.hku.hk>
2026-02-13 22:47:21 +0800

    update: `--notify` option

--- climatter/main.py
+++ climatter/main.py
@@ -6,13 +6,23 @@ from .event import Event
 from .get_events import read_events_from_file
 
 
-def load_events_with_args() -> list[Event]:
+def handle_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Event notifier and lister")
     parser.add_argument("events_file", help="Path to the events file")
     parser.add_argument(
-        "--dev-today", type=str, help="Override today's date (YYYY-MM-DD)"
+        "-n",
+        "--notify",
+        action="store_true",
+        help="Notify events. If not set, events will be listed instead.",
     )
-    args = parser.parse_args()
+
+    parser.add_argument(
+        "--dev-today", help="Override today's date (YYYY-MM-DD)"
+    )
+    return parser.parse_args()
+
+
+def load_events_with_args(args: argparse.Namespace) -> list[Event]:
     loaded_events = read_events_from_file(args.events_file)
     if args.dev_today:
         from datetime import datetime
@@ -31,7 +41,8 @@ def load_events_with_args() -> list[Event]:
 
 
 def main():
-    events = load_events_with_args()
+    args = handle_args()
+    events = load_events_with_args(args)
     events.sort(
         key=lambda e: (
             e.tdelta,
@@ -39,8 +50,10 @@ def main():
         ),
         reverse=True,
     )
-    notify_events(events)
-    list_events(events)
+    if args.notify_only:
+        notify_events(events)
+    else:
+        list_events(events)
 
 
 if __name__ == "__main__":
